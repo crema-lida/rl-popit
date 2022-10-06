a1d6aa31185f195418cd8a4dcb5861b58c830e56
crema <lida.2002@foxmail.com>
2022-10-06 18:42:15 +0800

    Update algorithm.

--- train.py
+++ train.py
@@ -11,14 +11,20 @@ from utils import SIZE, from_numpy, choice, augment_data
 
 
 def train_with_policy_network(epochs=10000):
-    model_idx = 1
+    model_idx = 6
+    opp_model = {}
+    for i in range(1, 21):
+        model_path = f'model/opp_{i}'
+        if os.path.exists(model_path):
+            opp_model[model_path] = torch.load(model_path)
+
     start = time()
     for ep in range(epochs + 1):
         if ep % 100 == 0:
             model_path = f'model/opp_{model_idx}'
             opp_model[model_path] = agent.state_dict()
             torch.save(opp_model[model_path], model_path)
-            model_idx = model_idx + 1 if model_idx < 10 else 1
+            model_idx = model_idx + 1 if model_idx < 20 else 1
         model_path = random.sample(list(opp_model.keys()), 1)[0]
         opponent.load_state_dict(opp_model[model_path])
 
@@ -107,6 +113,7 @@ def train_with_mcts(epochs=10000):
     cross_entropy = torch.nn.CrossEntropyLoss()
     start = time()
     for ep in range(epochs + 1):
+        opponent.load_state_dict(agent.state_dict())
         wins = 0
 
         for first_move in range(2):
@@ -114,6 +121,8 @@ def train_with_mcts(epochs=10000):
             state, done, rewards = env.reset()
             while np.any(~done):
                 state[:, [0, 1]] = state[:, [1, 0]]
+                state[:, 2] = np.where(state[:, 0] == SIZE, 1, 0)  # positions full of agent's pieces
+                state[:, 3] = np.where(state[:, 1] == SIZE, 1, 0)  # positions full of opponent's pieces
 
                 if player == 0:
                     idx = np.arange(len(state[~done]))
@@ -184,17 +193,15 @@ def train_with_mcts(epochs=10000):
             if player == 0: rewards = -rewards
             wins += 0.5 * (rewards.sum() + env.batch_size)
 
+        for net in agent.policy_network:
+            clip_grad_norm_(net.parameters(), max_norm=1.0, error_if_nonfinite=True)
         policy_optim.step()
         policy_optim.zero_grad()
 
         win_rate = wins / (2 * env.batch_size)
         print(f'Epoch {ep} | Win Rate: {win_rate * 100:.2f} % | '
               f'elapsed: {time() - start:.2f} s')
-
-        model_state = agent.state_dict()
-        cnn[1].load_state_dict(model_state)
-        torch.save(model_state, MODEL_PATH)
-        print('New model state saved!')
+        torch.save(agent.state_dict(), 'model/agent')
 
 
 def rollout(state, action):
@@ -202,6 +209,8 @@ def rollout(state, action):
     player = 1
     while np.any(~done):
         state[:, [0, 1]] = state[:, [1, 0]]
+        state[:, 2] = np.where(state[:, 0] == SIZE, 1, 0)  # positions full of agent's pieces
+        state[:, 3] = np.where(state[:, 1] == SIZE, 1, 0)  # positions full of opponent's pieces
 
         _state = from_numpy(state[~done], device)
         mask = _state[:, 1].reshape(-1, 36) != 0
@@ -225,6 +234,8 @@ def play_with_mcts():
     state, done, rewards = env.reset()
     while np.any(~done):
         state[:, [0, 1]] = state[:, [1, 0]]
+        state[:, 2] = np.where(state[:, 0] == SIZE, 1, 0)  # positions full of agent's pieces
+        state[:, 3] = np.where(state[:, 1] == SIZE, 1, 0)  # positions full of opponent's pieces
 
         if player == 0:
             idx = np.arange(len(state[~done]))
@@ -289,7 +300,7 @@ def play_with_mcts():
 if __name__ == '__main__':
     # os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
 
-    env = Env(graphics=False, fps=None, batch_size=512)
+    env = Env(graphics=False, fps=None, batch_size=128)
     device = torch.device('cuda')
     agent = Network().to(device)
     opponent = Network().to(device)
@@ -298,11 +309,6 @@ if __name__ == '__main__':
     if os.path.exists('model/agent'):
         model_state = torch.load('model/agent')
         agent.load_state_dict(model_state)
-    opp_model = {}
-    for i in range(10):
-        model_path = f'model/opp_{i}'
-        if os.path.exists(model_path):
-            opp_model[model_path] = torch.load(model_path)
 
     params = {'weight': [], 'bias': []}
     for net in agent.policy_network:
