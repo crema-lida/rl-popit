424b45d898072df258ec8b91c6102726a07a523a
Crema <lida.2002@foxmail.com>
2023-01-02 20:44:07 +0800

    Update Algorithm

--- src/onnx2tf.py
+++ src/onnx2tf.py
@@ -2,8 +2,9 @@ if __name__ == '__main__':
     import onnx
     from onnx_tf.backend import prepare
 
-    EXPORT_DIR = '../best_models/resnet3-128-old'
+    EXPORT_DIR = '../best_models/resnet3-64'
 
-    onnx_model = onnx.load(f'{EXPORT_DIR}/conv_block.onnx')
-    tf_model = prepare(onnx_model)
-    tf_model.export_graph(f'{EXPORT_DIR}/conv-block.tf')
+    for module in ['conv_block', 'policy_head']:
+        onnx_model = onnx.load(f'{EXPORT_DIR}/{module}.onnx')
+        tf_model = prepare(onnx_model)
+        tf_model.export_graph(f'{EXPORT_DIR}/{module}.tf')
